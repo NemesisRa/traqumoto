a5ca01ee86a2188edba6fddd4c84361c99d8ba38
Unknown <maxime.leiber@etu.emse.fr>
2017-04-20 09:44:57 +0200

    3e condition pour traitement

--- prediction_image_bcgsub.lua
+++ prediction_image_bcgsub.lua
@@ -20,20 +20,14 @@ l = 60
 L = 120
 
 net = torch.load('network.t7')
-
-vidname = 'Video/test2.mp4'
+vidname = 'Video/test.avi'
 vid = cv.VideoCapture{vidname}
+
 if not vid:isOpened() then
     print("Failed to open the video")
     os.exit(-1)
 end
 
---[[imgname = 'BDD/Image_à_tester/Motos04.PNG'
-Imgcoul = cv.imread{imgname}
-Img = cv.imread{imgname,cv.IMREAD_GRAYSCALE}
-
-width = Img:size()[1]
-length = Img:size()[2] ]]
 
 pMOG2 = cv.BackgroundSubtractorMOG2{}
 local _, frame = vid:read{}
@@ -44,102 +38,84 @@ for i=1,30 do
 	fgMaskMOG2 = pMOG2:apply{frame}
 end
 
+
 width = frame:size()[1]
 length = frame:size()[2]
 Img = torch.Tensor(width,length):zero()
 Img = cv.cvtColor{frame, nil, cv.COLOR_BGR2GRAY}
 
 cv.namedWindow{'win1'}
-cv.setWindowTitle{'win1', 'N&B'}
-cv.imshow{'win1', Img}
+cv.setWindowTitle{'win1', 'Couleur'}
 
 cv.namedWindow{'win2'}
 cv.setWindowTitle{'win2', 'Mask'}
-cv.imshow{'win2', fgMaskMOG2}
 
-cv.namedWindow{'win3'}
-cv.setWindowTitle{'win3', 'Couleur'}
-cv.imshow{'win3', frame}
-
-key=0
-while key~=27 and key~=113 do
-	if not(vid:read{frame}) then
+local pause = false
+local key = 0
+ 
+while true do
+	
+	if key == 32 then --space
+		key = 0
+		pause = not pause
+		print("en pause")
+		key=cv.waitKey{0}
+	elseif key == 27 or key == 113 then --escape or q
+		print("quitter")
 		break
 	end
-	fgMaskMOG2 = pMOG2:apply{frame}
-
-	Img = cv.cvtColor{frame, nil, cv.COLOR_BGR2GRAY}
-
-	for i=L/2+1,width-L/2,5 do
-		print(string.format('[%2.0f', i/(width-L/2)*100)..'%] Prédiction de l\'image')
-		for j=l/2+1,length-l/2,5 do
-			if fgMaskMOG2[i][j]>250 then
-				m=0
-				for k=-5,5 do
-					for l=-4,4 do
-						m=m+fgMaskMOG2[i+k][j+l]
-					end
-				end
-				m=m/99
-				if m>200 then
-					--cv.rectangle{frame, pt1={j-l/2, i-L/2}, pt2={j+l/2-1, i+L/2-1}, color = {255,0,0}}
-					sub = torch.Tensor(1,L,l):copy(Img:sub(i-L/2,i+L/2-1,j-l/2,j+l/2-1))
-					predicted = net:forward(sub:view(1,L,l))
-					--predicted = predicted:exp()
-					if predicted[1]==1 then
-						cv.rectangle{frame, pt1={j-l/2, i-L/2}, pt2={j+l/2-1, i+L/2-1}, color = {0,255,0}}
-					end
-				end
-			end
-		end
-	end
-
-	cv.imshow{'win1', Img}
-	cv.imshow{'win2', fgMaskMOG2}
-	cv.imshow{'win3', frame}
-	key=cv.waitKey{1}
 	
-	for i=1,2 do
+	if not pause then
+
 		if not(vid:read{frame}) then
+			print("Fin de la video")
+			key=cv.waitKey{0}
 			break
 		end
+
 		fgMaskMOG2 = pMOG2:apply{frame}
-	end
-end
-
-cv.waitKey{0}
-cv.destroyAllWindows{}
-
---[[width = frame:size()[1]
-length = frame:size()[2]
-Img = torch.Tensor(width,length):zero()
-Img = cv.cvtColor{frame, nil, cv.COLOR_BGR2GRAY}
-
-for i=L/2+1,width-L/2,5 do
-	print(string.format('[%2.0f', i/(width-L/2)*100)..'%] Prédiction de l\'image')
-	for j=l/2+1,length-l/2,5 do
-		if fgMaskMOG2[i][j] > 150 then
-			--cv.rectangle{frame, pt1={j-l/2, i-L/2}, pt2={j+l/2-1, i+L/2-1}, color = {255,0,0}}
-			sub = torch.Tensor(1,L,l):copy(Img:sub(i-L/2,i+L/2-1,j-l/2,j+l/2-1))
-			predicted = net:forward(sub:view(1,L,l))
-			predicted = predicted:exp()
-			if predicted[1]>0.9 then
-				cv.rectangle{frame, pt1={j-l/2, i-L/2}, pt2={j+l/2-1, i+L/2-1}, color = {0,255,0}}
+		Img = cv.cvtColor{frame, nil, cv.COLOR_BGR2GRAY}
+		for i=L/2+1,width-L/2,5 do
+			--print(string.format('[%2.0f', i/(width-L/2)*100)..'%] Prédiction de l\'image')
+			for j=l/2+1,length-l/2,5 do
+				if fgMaskMOG2[i][j] > 250 then
+					m=0
+					for k=-5,5 do
+						for l=-4,4 do
+							m=m+fgMaskMOG2[i+k][j+l]
+						end
+					end
+					m=m/99
+					if m>200 then
+						m = 0
+						n = 0
+						for k=-L/2,L/2 do
+							m=m+fgMaskMOG2[i+k][j-l/2]
+							n=n+fgMaskMOG2[i+k][j+l/2]
+						end
+						m=m/L
+						n=n/L
+						if m<50 and n<50 then
+							--cv.rectangle{frame, pt1={j-l/2, i-L/2}, pt2={j+l/2-1, i+L/2-1}, color = {255,255,255}}
+							sub = torch.Tensor(1,L,l):copy(Img:sub(i-L/2,i+L/2-1,j-l/2,j+l/2-1))
+							predicted = net:forward(sub:view(1,L,l))
+							--predicted = predicted:exp()
+							if predicted[1]==1 then
+								cv.rectangle{frame, pt1={j-l/2, i-L/2}, pt2={j+l/2-1, i+L/2-1}, color = {0,255,0}}
+							end
+						end
+					end
+				end
 			end
-		end
+		end			
+		cv.imshow{'win1', frame}
+		cv.imshow{'win2', fgMaskMOG2}
+		key=cv.waitKey{1} --en ms	
+		
 	end
 end
 
-cv.namedWindow{'win1'}
-cv.setWindowTitle{'win1', 'N&B'}
-cv.imshow{'win1', Img}
 
-cv.namedWindow{'win2'}
-cv.setWindowTitle{'win2', 'Mask'}
-cv.imshow{'win2', fgMaskMOG2}
 
-cv.namedWindow{'win3'}
-cv.setWindowTitle{'win3', 'Couleur'}
-cv.imshow{'win3', frame}
-cv.waitKey{0}
-cv.destroyAllWindows{}]]
+cv.destroyAllWindows{}
+os.exit(0)
